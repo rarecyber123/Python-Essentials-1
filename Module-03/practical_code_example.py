9a045e46bfea8d615cd069487a19d0a2cb72e530
khizra <rarember123@gmail.com>
2026-09-06 18:29:18 +0500

    program2_added

--- Module-03/practical_code_example.py
+++ Module-03/practical_code_example.py
@@ -1,1 +1,42 @@
 
+# 1. program
+# find_big_num
+
+number1 = int(input("Enter the first number: "))
+number2 = int (inout("Enter the second number: "))
+
+if number1 > number2:
+  larger_number = number1
+else: 
+  larger_number = number2 
+
+print("The larger number is:", larger_num)
+
+---
+
+# 2.program
+# find biggest 
+
+number1 = int(input("Enter the first number: "))
+number2 = int(input("Enter the second number: "))
+number3 = int(input("Enter the third number: "))
+# Pehle number1 ko sab se bara maan lete hain
+
+largest_number = number1
+if number2 > largest_number:
+  largest_number = number2
+
+if number3  > largest_number:
+  largest_number = number3
+
+print("The largest number is:", largest_number)
+
+---
+
+
+
+
+
+
+
+
